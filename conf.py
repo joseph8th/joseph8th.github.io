6d66633eab0d6b75e7d9c2795a3efdb262b16520
Joseph Edwards VIII <jedwards@cieloconnects.com>
2020-11-13 00:32:05 -0500

    Nikola auto commit.
    Nikola version: 8.1.1

--- conf.py
+++ conf.py
@@ -1389,9 +1389,8 @@ NAVIGATION_LINKS = {
         ('/archive.html', 'Archives', 'fa fa-folder-open'),
         ('/categories/index.html', 'Tags', 'fa fa-tags'),
         ('/rss.xml', 'RSS', 'fa fa-rss'),
-        ('https://getnikola.com', 'About me', 'fa fa-user'),
-        ('https://twitter.com/getnikola', 'My Twitter', 'fab fa-twitter'),
-        ('https://github.com/getnikola', 'My Github', 'fab fa-github'),
+        ('/about.html', 'About Me', 'fa fa-user'),
+        ('https://github.com/joseph8th', 'My Github', 'fab fa-github'),
     )
 }
 
